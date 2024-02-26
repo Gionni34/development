0223eb623203ca0db1b0dce23f758331fad3dcea
Gionni Young <gionninyoung@gmail.com>
2024-02-26 08:03:46 -0500

    updated file with dictionary

--- Class 2/variables_operators.py
+++ Class 2/variables_operators.py
@@ -39,42 +39,82 @@
 
 
 # Exponents
-print(5**5)
-print(2**5)
-print(3**6)
+# print(5**5)
+# print(2**5)
+# print(3**6)
 
 
 # Integer Division
-
+# print(10//3)
+# print(4//3)
+# print(5//2)
 
 # Modulo/Mod/Remainder
-
+# print(5 % 2)
+# print(10 % 4)
+# print(6 % 2)
 
 # Program to find the perimeter of a rectangle
 
+# perimeter = 2 * (length + width)
+
+# length = 10
+# width = 7
+
+# perimeter = 2 * (length + width)
+# print('The perimeter of this rectangle is', perimeter)
 
 # Data Types
 
 
 # Integer
-
+# num_one = 5
+# print(num_one)
+# print(type(num_one))
 
 # String
+# fav_color = 'blue'
+# print(fav_color)
+# print(type(fav_color))
 
 
 # Bool
-
+# technical_errors = True 
+# print(technical_errors)
+# print(type(technical_errors))
 
 # Float
+# num_two = 1.22
+# print(num_two)
+# print(type(num_two))
 
 
 # String
+# first_name = 'Gionni'
+# last_name = 'Young'
+# fullname = first_name + ' ' + last_name
+# print(fullname)
+# print(first_name, last_name)
+
+
 
 
 # List 
+# student_grades = [100, 95, 70, 85, 40]
+# print(student_grades)
+# print(type(student_grades))
 
+# For loop
+# for s in student_grades:
+#     print(s)
 
 # Dictionary
+demographic_info = {"First Name": "Gionni",
+                    "Last Name": "Young",
+                    "State": "New York"}
+
+print(demographic_info)
+print(type(demographic_info))
 
 
 # Cast a string to an integer
