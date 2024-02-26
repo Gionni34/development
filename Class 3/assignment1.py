4eec82a7273d7391f59df59f36c282ac800e74b3
Gionni Young <gionninyoung@gmail.com>
2024-02-26 11:29:40 -0500

    Calculating the perimeter of a triangle

---
+++ Class 3/assignment1.py
@@ -0,0 +1,33 @@
+# ''' This program calculates the perimeter of a triangle
+
+# Requirements
+#     - Comment your code 
+#         +5
+#     - Assign meaningful variable names 
+#         +5
+#     - Output a print statement, "The perimeter of your triangle is..." 
+#         +5
+#     - The test case for your program is side # 1 is 5, the base is 6, side # 2 is 10 
+#         +5
+#     - Output a print statement, "Is side 1 greater than side 2?" 
+#         +5
+#     - Output a print statement, "Is the base equal to side 1?"
+#         +5
+# '''
+
+
+
+#Calculating the perimeter of a triangle
+
+side_A = 5
+base = 6
+side_B = 10
+
+perimeter = side_A + base + side_B
+
+print("The perimeter of your triangle is", perimeter)
+print("Is side 1 greater than side 2?",side_A > side_B)
+print("Is the base equal to side 1?" , base == side_A)
+
+
+
