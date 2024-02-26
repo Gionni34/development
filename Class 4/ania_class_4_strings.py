8d0a2dfa5d38ab238f01c8b56145721315d256d3
Gionni Young <gionninyoung@gmail.com>
2024-02-26 11:40:10 -0500

    Strings

---
+++ Class 4/ania_class_4_strings.py
@@ -0,0 +1,144 @@
+''' Class 4 Strings '''
+
+# String operators
+
+'''Concatenation'''
+
+''' Multiplication '''
+
+
+# Reference vs Value equality == vs is
+x = 'hello'
+str2 = 'HELLO'.lower()
+
+
+''' in - Returns True if a string appears inside another string (as a substring), and False otherwise.'''
+
+test_character = 'b'
+test_string = 'bananas'
+
+
+''' create a quick test to see if the sub string 'spreh' can be found in the string 'Incomprehensibilities' '''
+test_char = 'spreh'
+test_word = 'Incomprehensibilities'
+
+
+
+''' len() - Returns the length of a string, aka the number of characters in a string.'''
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+
+
+# String methods
+
+word_1 = 'happy' #.capitalize
+
+
+ex_1 = 'cereal' # capitalize me!
+
+
+word_2 = 'SuPrISe' # .casefold
+
+
+ex_2 = 'RuMMaGe' # make me lower case!
+
+
+word_3 = 'ZOO'
+
+
+ex_3 = 'PLANET' # lets make this lower case?
+
+
+
+# isalnum() Are all my characters alphanumeric? Alphanumeric is A-Z, a-z and 0-9
+
+test_1 = 'abcdef'
+test_2 = '%$123'
+
+
+ex_8 = '123*' 
+
+# isalpha() Area all characters in the string in the alphabet?
+
+test_3 = 'abcde'
+test_4 = '012345'
+
+
+
+ex_9 = 'LMN0P' # Are we all in the alphabet
+
+# isdecimal() Are all characters decimals?
+
+test_5 = '1234P'
+test_6 = '234567'
+
+
+ex_11 = '123456' # Check for decimals?
+
+# isdigit() Are all characters digits?
+
+test_7 = 'H1234'
+test_8 = '9876'
+
+ex_10 = '123Hello' # Check for digits!
+
+
+# strip() Returns a trimmed version of the string
+username = '   jessica123    '
+
+
+ex_21 = '  sportsfan876  ' # sanitize this string
+
+'''
+Write some code that will take a string from the user and print if it is a number or not.
+
+Examples:
+apple
+False
+
+4
+True
+'''
+
+# Get input from user
+
+
+# Test input
+
+
+
+# Provide output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
