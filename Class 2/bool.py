3584cfc5ae5a7b960fb6258ed836bffeba6579f7
Gionni Young <gionninyoung@gmail.com>
2024-02-26 08:36:48 -0500

    boolean example assignment

--- Class 2/bool.py
+++ Class 2/bool.py
@@ -8,3 +8,17 @@ a brief python script that will determine the perimeter of the triangle. Comment
 4. Using boolean operators is base larger than or equal to side #1?
 
 '''
+
+
+# Variables
+
+side_one = 6
+base = 4
+side_two = 7
+
+# Formula
+perimeter = side_one + base + side_two
+print(perimeter)
+print('Using boolean operators is side #1 greater than the base?',side_one > base)
+print('Using boolean operators is side #2 less than the base?', side_two < base)
+print('Using boolean operators is base larger than or equal to side #1?',base >= side_one)
