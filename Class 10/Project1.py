c6ff5b1ea24e663aef34bc37d2c818bce91df692
Gionni Young <gionninyoung@gmail.com>
2024-03-13 18:52:43 -0400

    Working with list

--- Class 10/Project1.py
+++ Class 10/Project1.py
@@ -1,12 +1,10 @@
 username = input("To sign up please enter username that starts with a lowercase letter,""\n" "and only contain letters, numbers, and underscores: ")
 # password = input("Please enter password that is 8 characters long, contain at least one uppercase letter,""\n" "one lowercase letter, one digit, one special character, and not have any spaces: ")
   
-if username == "admin" or "admin123" or "superuser" or "superuser123": print("Username Taken")
-else: print("Invalid username")
-
-
-
-
+if username == "admin" and "admin123" and "superuser" and "superuser123" : 
+    print("Username Taken")
+else:
+    print("Invalid Username")
 
 
 
