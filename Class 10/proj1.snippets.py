16e4280d5c2a2766d637a8302ac892d4fab8b9fd
Gionni Young <gionninyoung@gmail.com>
2024-03-13 20:46:11 -0400

    Project 1 Help

--- Class 10/proj1.snippets.py
+++ Class 10/proj1.snippets.py
@@ -7,16 +7,23 @@ import re
 
 first_input, second_input = '',''
 
-while True:
-    first_input =input("Please enter your data: ")
-    second_input =input("Please enter your data: ")
+# while True:
+#     first_input =input("Please enter your data: ")
+#     second_input =input("Please enter your data: ")
     
-    print(first_input,second_input)
-    break
-
-
+#     print(first_input,second_input)
+#     break
 
 '''Handling error messages with a list (and testing)'''
+
+error_messages = ['Error message 1' , 'Error message 2', 'Error message 3']
+
+
+
+# if 5 > 7:
+#     print(error_messages[0])
+# else:
+#     print(error_messages[2])
 
 
 
@@ -25,44 +32,96 @@ while True:
 # Example of string testing (testing for lowercase letter)
 
 
+# test_string = 'Popeye1989'
+
+
 # Testing for uppercase as first letter
+
+# first_letter = test_string[0]
+# lowercase_test = first_letter.islower()
+
+# if lowercase_test:
+#     print("This string failed lowercase testing")
+# else:
+#     print("This string passed lowercase testing")
 
 
 
 
 ''' Taken usernames '''
 
+# sample_word = 'blue'
+# sample_list = ['green', 'blue', 'orange', 'yellow', 'purple']
+
+# if sample_word in sample_list:
+#     print("Words exist in list")
+# else:
+#     print("Word does not exist in list")
 
 
-''' Using Break and Continue to control the follow of loop'''
+
+''' Using Break and Continue to control the flow of loop'''
 
 # If input is a number we will go through this while loop and continue through, if not, we will send the user back to the beginning
 
 
+# while True:
+#     userinput =input("What is your test string? ")
+
+#     if userinput.isnumeric():
+#         print("This is a number, we will stay in the loop")
+#     else:
+#         print("Not a number, have the user try again. ")
+#         continue
+
+#     print("If you see this line of code, we are still in the loop")
+#     break
 
 ''' Password requirements '''
 
+test_string = 'c123456'
+# # At least 8 characters long
 
-# At least 8 characters long
+# # Contains at least one uppercase letter
+# one_uppercase = False
+# for t in test_string:
+#     if t.isupper():
+#         one_uppercase = True
 
-# Contains at least one uppercase letter
+# print("Contains at least one uppercase letter? ", one_uppercase)
 
 
 # Even better, is the any function! Tests if any of items in iterable is true
 
 
+# any_uppercase = any(u.isupper() for u in test_string)
+# print("Contains at least one uppercase letter?", any_uppercase)
+
+
 # Or Regular Expressions match method
+
+# uppercase_test_regex = bool(re.match(r'\w*[A-Z]\w*', test_string))
+# print("Contains at least one uppercase letter?", uppercase_test_regex)
 
 
 ''' Logging in and handling the loop'''
 
 # These can represent the user id and password the user created
-# sys_username = 'simonsays123'
-# sys_password = 'fido1950'
+sys_username = 'simonsays123'
+sys_password = 'fido1950'
 
 
 # These can represent the re-authentication
-# username = 'simonsays123'
-# password = 'fido1950'
+username = 'simonsays'
+password = 'fido1950'
 
 # Lets check for a match
+
+while True:
+    
+    if sys_username == username and sys_password == password:
+        print("Login Successful")
+        break
+    else:
+        print("Incorrect username or password")
+         # would add "continue" to prompt them back to the top
